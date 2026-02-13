6939d04022e7510fac7ce31fa2151fc0db2aa511
320-18\thinkodia <ejjang2030@gmail.com>
2026-02-13 16:25:56 +0900

    chore : vercel 10

--- app.py
+++ app.py
@@ -5,7 +5,15 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # 진짜 Flask 앱 가져오기
-from LMS.app import app
+from LMS.app import app, socketio
 
 # Vercel용 변수
-app = app
+
+if __name__ == '__main__':
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=int(os.getenv('FLASK_APP_PORT', 5000)),
+        debug=True,
+        allow_unsafe_werkzeug=True
+    )
