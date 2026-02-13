c446a4c4ca3fc706d327657daff54686f9562917
320-18\thinkodia <ejjang2030@gmail.com>
2026-02-13 16:21:13 +0900

    chore : vercel 설정 8

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# 이 파일은 Vercel이 실행할 '가짜' 시작점입니다.
-# 진짜 코드는 LMS 폴더 안에 있는 app.py를 불러와서 실행합니다.
-
 import sys
 import os
 
-# 현재 폴더(flaskLMS)를 파이썬 경로에 추가
+# 현재 위치를 경로에 추가 (LMS 폴더를 찾기 위해)
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# LMS 폴더 안에 있는 app.py에서 'app' 객체를 가져옴
+# LMS 폴더 안의 진짜 app.py에서 app 객체를 가져옴
 from LMS.app import app
-
-# (중요) Vercel은 이 'app' 변수를 찾아서 서버를 킵니다.
